63e724b6a2631cdfeb6c0778ec0cb084920a60f4
Cihan Incirligoz <incirligozc@gmail.com>
2020-12-20 21:08:13 +0300

    Yeni endpointler ve coffeeMerge

--- src/main/java/com/coffeever/coffeever/repository/CoffeBeanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coffeever.coffeever.repository;
-
-import com.coffeever.coffeever.model.CoffeeBean;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoffeBeanRepository extends JpaRepository<CoffeeBean,String> {
-}
